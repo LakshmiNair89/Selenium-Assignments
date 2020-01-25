9f4f6501fff69039792ffa4bf1fedd576a4f8b18
laksh <laksh@LAPTOP-FEJ087AA>
2020-01-24 21:57:09 -0800

    Print pattern

---
+++ EktaJava/src/Usflag.java
@@ -0,0 +1,40 @@
+
+public class Usflag {
+
+	public static void main(String[] args) {
+		
+	// TODO Auto-generated method stub
+		for(int k=0;k<=5;k++)	{
+	for(int i=0;i<=5;i++) {
+		System.out.print("* ");
+		
+		}
+	for(int a=0;a<=38;a++) {
+		System.out.print("=");
+	}System.out.println();
+		for(int j=0;j<=4;j++) {
+			System.out.print("* ");
+		}
+		for(int a=0;a<=39;a++) {
+			System.out.print("=");}
+		System.out.println();
+	}
+		for(int l=0;l<=5;l++) {
+		for(int b=0;b<=49;b++) {
+			System.out.print("=");
+		}System.out.println();	
+		}
+	
+	
+
+String p1 = "* * * * * * ==================================\n * * * * *  ==================================";
+String p2 = "==============================================";
+for (int f = 0; f < 4; f++) {
+ System.out.println(p1);
+}
+System.out.println("* * * * * * ==================================");
+for (int g = 0; g < 6; g++) {
+ System.out.println(p2);
+}
+}
+}
