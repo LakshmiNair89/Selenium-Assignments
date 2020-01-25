428d4db7212bbc5e1ea1cbd3f605cfe13c2bc7b8
laksh <laksh@LAPTOP-FEJ087AA>
2020-01-24 22:00:33 -0800

    Get Input--- Scanner

---
+++ EktaJava/src/GetInput.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class GetInput {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc = new Scanner(System.in);
+int s= sc.nextInt();
+int s1= sc.nextInt();
+int s2= sc.nextInt();
+String s3=sc.next();
+System.out.println(s);
+System.out.println(s1);
+System.out.println(s2);
+System.out.println(s3);
+	}
+
+}
